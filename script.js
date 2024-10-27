c1705a0986e9a19f2b1523814b5a8c73aad9783e
drenxhyliqi <drenxhyliqi3@gmail.com>
2024-10-27 02:27:48 +0100

    Perfundimi I disa seksioneve

--- script.js
+++ script.js
@@ -34,4 +34,24 @@ document.addEventListener("DOMContentLoaded", function () {
     sr.reveal(".porsche_txt", { delay: 300 });
     sr.reveal(".electricp", { delay: 200 });
     sr.reveal(".main_img", { delay: 100 });
+    sr.reveal(".stat-item", { delay: 200 });
+    sr.reveal(".start_button", { delay: 200 });
+    sr.reveal(".about_porsche", { delay: 200 });
+    sr.reveal(".box_4", { delay: 900 });
+    sr.reveal(".about_content", { delay: 200 });
+    sr.reveal(".about_btn", { delay: 500 });
+    sr.reveal(".carousel-inner", { delay: 600 });
+    sr.reveal(".popular_content", { delay: 500 });
+    sr.reveal(".featured_content", { delay: 500 });
+    sr.reveal(".box_1", { delay: 300 });
+    sr.reveal(".box_2", { delay: 700 });
+    sr.reveal(".box_3", { delay: 1000 });
+    sr.reveal(".features", { delay: 700 });
 });
+
+
+
+// document.getElementById('playButton').addEventListener('click', function() {
+//     const audio = document.getElementById('audio');
+//     audio.play();
+//     });
